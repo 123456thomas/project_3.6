7653cae2e3877bc297865da67125011d78a452ca
123456thomas <17625809083@sina.cn>
2019-01-13 17:52:58 +0800

    python3.6环境的联系

--- Project_3.6/spider112/work01.py
+++ Project_3.6/spider112/work01.py
@@ -23,3 +23,42 @@ __mtime__ = '2018/9/25'
 爬取新闻标题，url，keywords
 http://sports.163.com/zc/
 """
+
+import re, requests
+import pymysql
+
+def Web_get(urls):
+    """
+    获取页面的源码
+    :param urls:
+    :return:
+    """
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+    response = requests.get(urls, headers=headers)
+    html = response.text
+    print(html)
+    return html
+
+def Spider_data(html):
+    """
+    抓取数据
+    :param html:
+    :return:
+    """
+
+def main():
+    baseurl = "http://sports.163.com/zc/"
+    # 翻页
+    begins = int(input("起始页:"))
+    ends = int(input("终止页页:"))
+    nums = 0
+    # 获取页面
+    for i in range(begins, ends + 1):
+        newurl = baseurl + str(i)
+        html = Web_get(baseurl)
+
+
+
+if __name__ == '__main__':
+    main()
+
